44007ca72d7717ce19d81455182aace32e2a3543
AmoraAmora <laamora0gmail.com>
2021-03-30 17:28:43 +0700

    problem

--- src/App.tsx
+++ src/App.tsx
@@ -4,50 +4,61 @@ import { useQuery } from '@apollo/client';
 import './App.css';
 import { CHAR_GET } from './character'
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
-import {  CharactersData, CharactersVars } from './COMPONENTS/interfaces';
+import {  CharactersData, CharactersVars, chars } from './COMPONENTS/interfaces';
 import TodoForm from './COMPONENTS/TodoForm';
+import { resultKeyNameFromField } from '@apollo/client/utilities';
 
 
-const client = new ApolloClient({
-  uri: 'https://rickandmortyapi.com/graphql',
-  cache: new InMemoryCache()
-});
 
-function App() {
+const App: React.FC = () => {
   const [todos, setTodos] = useState([])
 
   const { loading, data } = useQuery<CharactersData, CharactersVars>(
     CHAR_GET,
     { variables: { page: 1 } }
   );
+  
+  let chars = data! && data.characters.results.map<chars>(result =>(
+    {
+      jsx: <div className="Character">
+        <img src={result.image} alt={result.name} />
+        <button />
+      </div>,
+      name: result.name,
+    }
+    ))
+
+  /*{data&&data.characters.results.map( result => (
+      <div className="Character">
+           <img src={result.image} alt={result.name
+          } />
+      </div>)
+    )}
+    
+    
+        {chars!.map(char=> char.jsx)}*/
 
   const addHandler = (title:string) =>{
     console.log('add new', title);
-    var char = data&&data.characters.results.find(item => item.name == title);
-    console.log(char!.image)
-    //return (<div className="Character">
-    //           <img src={char!.image} />
-    //</div>);
+    var char = chars!.find(item => item.name.indexOf(title) != -1);
+    console.log(char!.jsx)
 
   }
 
   return (    
-    <ApolloProvider client={client}>
       <div className="App">
         <TodoForm onAdd={addHandler}/>
              
 
         <div className="Characters">
-          {data&&data.characters.results.map( result => (
-          <div className="Character">
-               <img src={result.image} alt={result.name
-              } />
-          </div>)
-          )}
-          
+        {data&&data.characters.results.map( result => (
+      <div className="Character">
+           <img src={result.image} alt={result.name
+          } />
+      </div>)
+    )}
         </div>
       </div>
-    </ApolloProvider>
   );
 }
 
--- src/COMPONENTS/interfaces.ts
+++ src/COMPONENTS/interfaces.ts
@@ -2,15 +2,17 @@ type Character = {
     name: string;
     image: string;
 }
-
 export type Characters = {
     results:Character[];
 }
-
 export type CharactersData = {
     characters:Characters;
 }
-
 export interface CharactersVars {
     page: number;
 };
+
+export interface Chars{
+    jsx: JSX.Element
+    name: string
+  }[]
